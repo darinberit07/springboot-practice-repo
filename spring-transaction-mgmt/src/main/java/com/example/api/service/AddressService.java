a67a918638a15328ca3fe4b1d2da41bf50d400b9
Darin Berit R S <darinberitrs@gmail.com>
2024-07-18 16:47:39 +0530

    Added a propagation level for Address Service

--- spring-transaction-mgmt/src/main/java/com/example/api/service/AddressService.java
+++ spring-transaction-mgmt/src/main/java/com/example/api/service/AddressService.java
@@ -2,6 +2,8 @@ package com.example.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.api.model.Address;
 import com.example.api.repository.AddressRepository;
@@ -12,6 +14,7 @@ public class AddressService {
     @Autowired
     private AddressRepository addressRepository;
     
+    @Transactional(propagation = Propagation.REQUIRED)
     public Address addAddress(Address address) {
         Address savedAddress = addressRepository.save(address);
         return savedAddress;
